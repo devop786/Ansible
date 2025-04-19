3a73909e9d41090f4ca6ac849ff0838f5242f5ca
Ubuntu <ubuntu@ip-172-31-1-228.eu-north-1.compute.internal>
2025-04-19 17:19:56 +0000

    chnages for playbook

--- playbook/kubeadmsetup/kube_join_command.sh
+++ playbook/kubeadmsetup/kube_join_command.sh
@@ -1,1 +1,4 @@
-kubeadm join 172.31.39.11:6443 --token eektdo.lgl497saknd8oxrx --discovery-token-ca-cert-hash sha256:6c845c0ae35c4f6fb6aef20ed4390888b42babf9e58f32b17c01f5e204bf2d08 
+#!/bin/bash
+kubeadm join 172.31.1.228:6443 --token lacx3d.186pwi3p4opq7sv2 \
+  --discovery-token-ca-cert-hash sha256:2ff6cbc30dc4d95033657ef009cdaa7b76dcf3b630a3500376034b595ae9a25a \
+  --cri-socket unix:///var/run/cri-dockerd.sock
